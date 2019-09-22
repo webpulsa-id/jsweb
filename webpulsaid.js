3e0e6a1aba0649709ef00aea277ae767b0c71968
webpulsa-id <53376749+webpulsa-id@users.noreply.github.com>
2019-09-22 11:34:32 +0700

    Update webpulsaid.js

--- webpulsaid.js
+++ webpulsaid.js
@@ -50,7 +50,7 @@ li[i].style.display = "none";
 			return jackpot;
 		}
 		function addCommas(nStr) {
-			nStr += 'Member Aktif : ±';
+			nStr += ' Member Aktif.';
 			x = nStr.split('.');
 			x1 = x[0];
 			x2 = x.length > 1 ? '.' + x[1] : '';
