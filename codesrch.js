0383d2c5a359f6eb4a3b2084366c810e983e99d9
webpulsa-id <53376749+webpulsa-id@users.noreply.github.com>
2019-10-06 23:58:40 +0700

    Update codesrch.js

--- codesrch.js
+++ codesrch.js
@@ -18,7 +18,21 @@ if (uri.indexOf("?m=1","?m=1") > 0) {
   var clean_uri = uri.substring(0, uri.indexOf("?m=1"));
     window.history.replaceState({}, document.title, clean_uri);
 }
-
+var uri = window.location.toString();
+if (uri.indexOf("/p/harga.html","/p/harga.html") > 0) {
+  var clean_uri = uri.substring(0, uri.indexOf("/p/harga.html"));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf("/p/webreport.html","/p/webreport.html") > 0) {
+  var clean_uri = uri.substring(0, uri.indexOf("/p/webreport.html"));
+    window.history.replaceState({}, document.title, clean_uri);
+}
+var uri = window.location.toString();
+if (uri.indexOf("/p/cetak-struktur.html","/p/cetak-struktur.html") > 0) {
+  var clean_uri = uri.substring(0, uri.indexOf("/p/cetak-struktur.html"));
+    window.history.replaceState({}, document.title, clean_uri);
+}
 
 $(function() {
     $('span.open').click(function() {
