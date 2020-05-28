869b0c3b0ea92c6ef619d593d407fff5571bc0d5
webpulsa-id <53376749+webpulsa-id@users.noreply.github.com>
2020-05-28 19:40:35 +0700

    Add files via upload

--- mp3.js
+++ mp3.js
@@ -159,7 +159,7 @@ jQuery(function($) {
             trackNumber +
             '.</div><div class="plTitle">' +
             trackName +
-            '</div><div class="plLength">' +
+            '</div><br/><div class="plLength">' +
             trackLength +
             "</div></div></li>"
         );
