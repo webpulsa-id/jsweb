2de70ed9a760364ee88d4c0efef4a52e80940b6d
webpulsa-id <53376749+webpulsa-id@users.noreply.github.com>
2020-06-27 14:59:22 +0700

    Add files via upload

--- M-ListPopindo.js
+++ M-ListPopindo.js
@@ -20,72 +20,78 @@ jQuery(function($) {
       tracks = [
         {
           track: 1,
+          file: "nano-separuhku/Nano-Separuhku",
+          name: "Nano Separuhku | Cinta suci",
+          length: "ilir 7"
+        },
+        {
+          track: 3,
           file: "ilir-7-2019/ilir-7-02",
           name: "Cinta Terlarang",
           length: "ilir 7"
         },
         {
-          track: 2,
+          track: 4,
           file: "ilir-7-2019/ilir-7-01",
           name: "Salah Apa Aku",
           length: "ilir 7"
         },
         {
-          track: 3,
+          track: 5,
           file: "ilir-7-2019/ilir-7-03",
           name: "Honey",
           length: "ilir 7"
         },
         {
-          track: 4,
+          track: 6,
           file: "ilir-7-2019/ilir-7-04",
           name: "Jangan Kau Coba",
           length: "ilir 7"
         },
         {
-          track: 5,
+          track: 7,
           file: "ilir-7-2019/ilir-7-05",
           name: "Jangan Nakal Sayang",
           length: "ilir 7"
         },
         {
-          track: 6,
+          track: 8,
           file: "ilir-7-2019/ilir-7-06",
           name: "Kekasih Gelap",
           length: "ilir 7"
         },
         {
-          track: 7,
+          track: 9,
           file: "ilir-7-2019/ilir-7-07",
           name: "Lemah Letih Lesu",
           length: "ilir 7"
         },
         {
-          track: 8,
+          track: 10,
           file: "ilir-7-2019/ilir-7-08",
           name: "Sakit Sungguh Sakit",
           length: "ilir 7"
         },
         {
-          track: 9,
+          track: 11,
           file: "ilir-7-2019/ilir-7-09",
           name: "Sandiwara",
           length: "ilir 7"
         },
         {
-          track: 10,
+          track: 12,
           file: "ilir-7-2019/ilir-7-10",
           name: "Sella Selli",
           length: "ilir 7"
         },
         {
-          track: 11,
+          track: 13,
           file: "ilir-7-2019/ilir-7-11",
           name: "Hormati Kekasih Mu",
           length: "ilir 7"
         },
         {
-          track: 12,
+          track: 14,
           name: "Cinta itu kamu",
           length: "Geisha",
           file: "webpulsa/GEISHA%20-%20Cinta%20Itu%20Kamu"
